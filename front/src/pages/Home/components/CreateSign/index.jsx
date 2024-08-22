d363049fb3efdb6452fa8f56f594e1ca4f4b346b
azerkazimov <648azer@gmail.com>
2024-08-22 10:07:39 +0400

    how to use

--- front/src/pages/Home/components/CreateSign/index.jsx
+++ front/src/pages/Home/components/CreateSign/index.jsx
@@ -1,92 +1,106 @@
-import { useEffect, useRef } from "react";
-import "./_style.scss";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
-import { Link } from "react-router-dom";
 import SectionHeader from "../../../../components/common/SectionHeader";
+import design from "../../../../assets/images/createsign/design_signature.png";
+import integrate from "../../../../assets/images/createsign/integrate_signature.png";
+import boost from "../../../../assets/images/createsign/boost.png";
+import line from "../../../../assets/images/createsign/Line_17.png";
+import { Link } from "react-router-dom";
 
-gsap.registerPlugin(ScrollTrigger);
+import "./_style.scss";
 
 const CreateSign = () => {
-  const bannerRef = useRef([]);
-
-  const bannerData = [
-   
-    {
-      title: "Design",
-      item:"Design your signatures effortlessly with the easiest tool",
-      note:"Easily create your email signatures using our provided templates, with no additional design skills required! Showcase your brand identity in every email you send.",
-      button: "Create your signature",
-      className: "bg-primary60",
-      path: "/sign-up", 
-    },
-    {
-      title: "Design",
-      item:"Integrate and Manage Your Email Signatures",
-      note:"Effortlessly connect your email signatures with Gmail. Your team members can easily link their signatures to their accounts, update existing ones, and switch to new signatures as needed. Simplify your workflow with our intuitive tools!",
-      className: "bg-primary30",
-    },
-    {
-      title: "Design",
-      item:"Elevate Your Marketing Campaigns Using Signature Banners",
-      note:"Increase the impact of your emails by adding eye-catching banners to your email signatures. These banners not only make your brand more visible but also boost engagement and conversions. Customize them to fit your campaigns and see your marketing results improve",
-      className: "bg-primary",
-     
-      path: "#",
-    },
-  ];
-
-  useEffect(() => {
-    bannerRef.current.forEach((elem, index) => {
-      if (index === bannerRef.current.length - 1) return;
-
-      ScrollTrigger.create({
-        trigger: elem,
-        start: "top top",
-        end: "bottom top",
-        pin: true,
-        pinSpacing: false,
-      });
-    });
-
-    return () => ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-  }, []);
-
   return (
     <div className="create-signature">
-      <div className="container create-banners">
+      <div className="create-signature-banners">
         <SectionHeader header="How to use" />
-        {bannerData.map((banner, index) => (
-          <div
-            className={`container banner ${banner.className}`}
-            key={index}
-            ref={(elem) => (bannerRef.current[index] = elem)}
-          >
-            <div className="banner-header">
-              <h3>{banner.title}</h3>
-            </div>
-            <div className="row banner-content">
-              <div className="col-12 col-md-6 banner-description">
-                <h4>{banner.item}</h4>
-                <p className="">
-                  {banner.note}
+        <div className="container design">
+          <div className="col-12">
+            <Link to="#">
+              <button className="btn border-primary text-primary">
+                1. Design
+              </button>
+            </Link>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6 design-description">
+              <div className="col-9 design-description-text">
+                <h2>
+                  Design your signatures effortlessly with the easiest tool
+                </h2>
+                <p>
+                  Easily create your email signatures using our provided
+                  templates, with no additional design skills required! Showcase
+                  your brand identity in every email you send.
                 </p>
-                {banner.button && (
-                  <Link to={banner.path}>
-                    <button className="btn btn-primary text-natural">
-                      {banner.button}
-                    </button>
-                  </Link>
-                )}
               </div>
-              <div className="col-12 col-md-6 banner-template">
-                <div className="template-box">
-                  <p>Template design</p>
-                </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <img src={design} alt="Design your signature" />
+            </div>
+          </div>
+        </div>
+
+        <div className="line">
+          <img src={line} alt="line" />
+        </div>
+
+        <div className="container design">
+          <div className="col-12 flex-container flex-justify-end">
+            <Link to="#">
+              <button className="btn border-primary text-primary">
+                2. Integrate
+              </button>
+            </Link>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <img src={integrate} alt="Design your signature" />
+            </div>
+            <div className="col-12 col-md-6 design-description flex-container flex-justify-center">
+              <div className="col-9 design-description-text">
+                <h2>Integrate and Manage Your Email Signatures</h2>
+                <p>
+                  Effortlessly connect your email signatures with Gmail. Your
+                  team members can easily link their signatures to their
+                  accounts, update existing ones, and switch to new signatures
+                  as needed. Simplify your workflow with our intuitive tools!
+                </p>
               </div>
             </div>
           </div>
-        ))}
+        </div>
+
+        <div className="line">
+          <img src={line} alt="line" />
+        </div>
+
+        <div className="container design">
+          <div className="col-12">
+            <Link to="#">
+              <button className="btn border-primary text-primary">
+                3.Boost email campaigns
+              </button>
+            </Link>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6 design-description">
+              <div className="col-9 design-description-text">
+                <h2>
+                  Elevate Your Marketing Campaigns Using Signature Banners
+                </h2>
+                <p>
+                  Increase the impact of your emails by adding eye-catching
+                  banners to your email signatures. These banners not only make
+                  your brand more visible but also boost engagement and
+                  conversions. Customize them to fit your campaigns and see your
+                  marketing results improve
+                </p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <img src={boost} alt="Design your signature" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
