b00f33784f9c100e73d71d62ccf7883f6bf8a971
Leman-Mustafazadeh <sonaja@code.edu.az>
2024-09-04 12:47:03 +0400

    account

--- front/src/pages/AccountMember/index.jsx
+++ front/src/pages/AccountMember/index.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import "./_style.scss"
+import React from "react";
+import "./_style.scss";
 import logo from "../../assets/images/logo/logo.png";
-import {Link} from "react-router-dom"
+import account from "../../assets/images/accountmember/account.png";
+import { Link } from "react-router-dom";
 const AccountMember = () => {
   return (
     <section>
-        <div className="container">
+      <div className="container member">
         <div className="delete row flex-align-center flex-justify-space-between">
           <div className="row flex-align-center delete_head">
             <div className="delete_img">
@@ -65,14 +66,161 @@ const AccountMember = () => {
           </div>
         </div>
 
-        <div className="delete_wrap">
-          <div className="del_hero">
-            
+        <div className="account_wrap">
+          <div className="account_head">
+            <div className="account_hero row flex-align-center">
+              <div>
+                <img src={account} alt="" />
+              </div>
+              <div>
+                <h2 className="font-size-24 font-weight-500 text-text">
+                  aysuqulamaliyeva05@gmail.com
+                </h2>
+                <p className="font-size-20 font-weight-400 text-text">(user)</p>
+              </div>
+            </div>
+
+            <div className="account_del mt-9">
+              <button className="bg-primaryFAQ font-size-20 font-weight-400 text-text">
+                Account
+              </button>
+              <button className="bg-natural font-size-20 font-weight-400 text-text">
+                Settings
+              </button>
+            </div>
+          </div>
+
+          <div className="row account_upgrade_head gap-4">
+            <div className="col-12 col-md-6 account_upgrade">
+              <div>
+                <h2 className="font-size-18 font-weight-500 text-text10 ">
+                  Account Details
+                </h2>
+                <div className="py-4 row">
+                  <h4 className="font-size-16 font-weight-500 text-text60">
+                    Customer ID:
+                  </h4>
+                  <p className="font-size-16 font-weight-400 text-text">
+                    00128909
+                  </p>
+                </div>
+
+                <div className="pb-4 row">
+                  <h4 className="font-size-16 font-weight-500 text-text60">
+                    Signup date:
+                  </h4>
+                  <p className="font-size-16 font-weight-400 text-text">
+                    Jul 20, 2024
+                  </p>
+                </div>
+
+                <div className="pb-4 row">
+                  <h4 className="font-size-16 font-weight-500 text-text60">
+                    Account plan:
+                  </h4>
+                  <p className="font-size-16 font-weight-400 text-text">
+                    Free Trial (Expired)
+                  </p>
+                  <Link
+                    to={""}
+                    className="font-size-16 font-weight-400 text-primary"
+                  >
+                    Upgrade
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-6 account_upgrade ">
+              <div className="">
+                <h2 className="font-size-18 font-weight-500 text-text10 mb-5">
+                  Account Storage
+                </h2>
+
+                <div className="row flex-align-center mb-3">
+                  <p className="font-size-16 font-weight-500 text-text60 template">
+                    Template:
+                  </p>
+                  <div className="row flex-align-center ">
+                    <svg
+                      width="167"
+                      height="6"
+                      viewBox="0 0 167 6"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect width="167" height="6" rx="3" fill="#3C3C3C" />
+                      <rect
+                        width="167"
+                        height="6"
+                        rx="3"
+                        fill="white"
+                        fill-opacity="0.9"
+                      />
+                    </svg>
+
+                    <span className="pl-4">0 / 10</span>
+                  </div>
+                </div>
+                <div className="row pb-7">
+                  <p className="font-size-16 font-weight-500 text-text60 template">
+                    Members:
+                  </p>
+                  <div className="row flex-align-center">
+                    <svg
+                      width="167"
+                      height="6"
+                      viewBox="0 0 167 6"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect width="167" height="6" rx="3" fill="#3C3C3C" />
+                      <rect
+                        width="167"
+                        height="6"
+                        rx="3"
+                        fill="white"
+                        fill-opacity="0.9"
+                      />
+                      <rect width="42" height="6" rx="3" fill="#1A73E8" />
+                    </svg>
+
+                    <span className="pl-4">3 / 10</span>
+                    <Link
+                      to={""}
+                      className="font-size-16 font-weight-400 text-primary"
+                    >
+                      Add users
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="personal">
+            <h2 className="font-size-18 font-weight-500 text-text10 mb-5">
+              Personal Information
+            </h2>
+            <h3 className="font-size-16 font-weight-500 text-text60 pb-2">
+              Login Email:
+            </h3>
+            <div>
+              <input type="text" placeholder="kenantagiyev010@gmail.com" className="font-size-14 font-weight-400 text-text "/>
+              <Link to={""} className="font-size-14 font-weight-400 text-text60">Change email</Link>
+            </div>
+            <h3 className="font-size-16 font-weight-500 text-text60 pb-2 pt-4">
+              Password:
+            </h3>
+            <div>
+              <input type="password" placeholder="-----" />
+              <Link to={""} className="font-size-14 font-weight-400 text-primary ">Set a password</Link>
+            </div>
           </div>
         </div>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default AccountMember
+export default AccountMember;
