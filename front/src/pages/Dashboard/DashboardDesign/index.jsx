dd0bb4f5363546478bfe5dd3a5c24a29c2184e11
azerkazimov <648azer@gmail.com>
2024-10-23 09:52:30 +0400

    fix: dashboard banner style

--- front/src/pages/Dashboard/DashboardDesign/index.jsx
+++ front/src/pages/Dashboard/DashboardDesign/index.jsx
@@ -106,8 +106,8 @@ function DashboardDesign() {
   const iconColor = "#1251A3";
   const setIconColor = () => {
     if (
-      formValues.backgroundColor === "#ffffff" &&
-      formValues.fontColor === "#000000"
+      formValues.backgroundColor === "#fff" &&
+      formValues.fontColor === "#3C3C3C"
     ) {
       return iconColor;
     }
